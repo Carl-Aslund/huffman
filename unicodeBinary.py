ede6483e85d15847710fed7fbdbf1247e1871145
Carl Aslund <caslund@g.hmc.edu>
2018-02-17 18:10:06 -0800

    Add method signatures for binary-Unicode conversions

---
+++ unicodeBinary.py
@@ -0,0 +1,33 @@
+from config import CHR_BITS
+
+def binToDec(bin):
+    """Converts a binary string into a decimal number.
+
+    @param bin: a string of 1's and 0's
+    @return: an integer equivalent
+    """
+    pass
+
+def decToBin(dec):
+    """Converts a decimal number into a binary string.
+
+    @param dec: an integer in base-10
+    @return: a binary-string equivalent
+    """
+    pass
+
+def uniToBin(char):
+    """Converts a Unicode character into a binary string.
+
+    @param char: a unicode character (must not require more bits than available)
+    @return: an equivalent binary string
+    """
+    pass
+
+def binToUni(bin):
+    """Converts a binary string into a Unicode character.
+
+    @param char: a binary string
+    @return: an equivalent Unicode character
+    """
+    pass
