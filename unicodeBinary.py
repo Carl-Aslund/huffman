5288c3cd661553b7a086a3e88202cca3ef5644f2
Carl Aslund <caslund@g.hmc.edu>
2018-02-18 08:35:27 -0800

    Add unicode-binary conversions

--- unicodeBinary.py
+++ unicodeBinary.py
@@ -1,12 +1,15 @@
 from config import CHR_BITS
 
-def binToDec(bin):
+def binToDec(binStr):
     """Converts a binary string into a decimal number.
 
     @param bin: a string of 1's and 0's
     @return: an integer equivalent
     """
-    pass
+    decimal = 0
+    for digit in binStr:
+        decimal = 2 * decimal + int(digit)
+    return decimal
 
 def decToBin(dec):
     """Converts a decimal number into a binary string.
@@ -14,7 +17,16 @@ def decToBin(dec):
     @param dec: an integer in base-10
     @return: a binary-string equivalent
     """
-    pass
+    binStr = ""
+    i = 1
+    while dec > 0:
+        if (dec % 2**i) != 0:
+            binStr = "1" + binStr
+            dec -= 2**(i-1)
+        else:
+            binStr = "0" + binStr
+        i += 1
+    return "0"*(CHR_BITS-len(binStr)) + binStr
 
 def uniToBin(char):
     """Converts a Unicode character into a binary string.
@@ -22,12 +34,14 @@ def uniToBin(char):
     @param char: a unicode character (must not require more bits than available)
     @return: an equivalent binary string
     """
-    pass
+    decimal = ord(char)
+    return decToBin(decimal)
 
-def binToUni(bin):
+def binToUni(binStr):
     """Converts a binary string into a Unicode character.
 
     @param char: a binary string
     @return: an equivalent Unicode character
     """
-    pass
+    decimal = binToDec(binStr)
+    return chr(decimal)
