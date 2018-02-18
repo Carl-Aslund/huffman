d4b5137d49121c099a231f626b7993f53555c76f
Carl Aslund <caslund@g.hmc.edu>
2018-02-18 09:01:03 -0800

    Add method signatures for decoding

---
+++ decoder.py
@@ -0,0 +1,38 @@
+from config import CHR_BITS
+
+def readFile(fileName):
+    """Reads a specified text file.
+
+    @param fileName: a string containing the relative path of the target file
+    @return: a string containing the entire contents of that text file
+    """
+    pass
+
+def writeFile(fileName, text):
+    """Writes some text to a specified file.
+
+    @param fileName: a string containing the relative path of the target file
+    @param text: a string containing the text to be written
+    """
+    pass
+
+def readOverhead(binText):
+    """Processes the overhead of a Huffman-encoded file.
+
+    @param binText: the binary string obtained from converting the encoded file
+    @return: a dictionary containing the code
+    @return: the portion of the binary string containing the
+    """
+    pass
+
+def decode(codeDict, binText):
+    """Converts prefix code into its plaintext equivalent.
+
+    @param codeDict: a dictionary containing the prefix code
+    @param binText: a string containing the encoded text
+    @return: a string representing the original plaintext
+    """
+    pass
+
+if __name__ == "__main__":
+    # TODO: Write everything
