ac699782788be5d85032f51749367dfdb4c70580
Carl Aslund <caslund@g.hmc.edu>
2018-02-19 15:50:56 -0800

    Add overhead interpreter

--- decoder.py
+++ decoder.py
@@ -1,4 +1,6 @@
 from config import CHR_BITS
+from unicodeBinary import binToDec, textToBin
+from math import log2, ceil
 
 def readFile(fileName):
     """Reads a specified text file.
@@ -27,18 +29,40 @@ def readOverhead(binText):
 
     @param binText: the binary string obtained from converting the encoded file
     @return: a dictionary containing the code
-    @return: the portion of the binary string containing the
+    @return: the portion of the binary string containing the encoded message
     """
-    pass
+    frontBits = ceil(log2(CHR_BITS)) # Bits needed at front for zeroes
+    binText = textToBin(binText)
+    frontZeroes = binText[:frontBits]
+    numZeroes = binToDec(frontZeroes) # Provides the number of ending zeroes
+    leftoverText = binText[frontBits:] # Portion of text without leading bits
+    i = 0
+    codeDict = {}
+    for charInt in range(2**CHR_BITS):
+        numBitsBits = leftoverText[i:i+CHR_BITS]
+        numBits = binToDec(numBitsBits)
+        i += 8
+        if numBits == 0:
+            continue
+        else:
+            codeDict[chr(charInt)] = leftoverText[i:i+numBits]
+            i += numBits
+    encodedText = binText[i:-numZeroes]
+    return codeDict, encodedText
 
 def decode(codeDict, binText):
     """Converts prefix code into its plaintext equivalent.
 
     @param codeDict: a dictionary containing the prefix code
-    @param binText: a string containing the encoded text
+    @param binText: a binary string containing the encoded text
     @return: a string representing the original plaintext
     """
     pass
 
+def main():
+    name = input("Enter the name of the compressed file: ")
+    contents = readFile("compressed/"+name+".huff")
+    codeD, encoded = readOverhead(contents)
+
 if __name__ == "__main__":
-    # TODO: Write everything
+    main()
